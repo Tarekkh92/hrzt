94812b3bd58b4447fd99ffd73ceea974456613a6
tarek khalaily <khalailyt@gmail.com>
2021-02-02 15:10:07 +0200

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import * as $ from 'jquery';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
