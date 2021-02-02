17ad6271129436faf57c2b2e655fd70386349876
tarek khalaily <khalailyt@gmail.com>
2021-02-02 14:35:43 +0200

    make it better

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/hrzt'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/hrzt/index.html'));});
+app.listen(process.env.PORT || 8080);
